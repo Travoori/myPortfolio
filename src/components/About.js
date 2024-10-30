363f8ffed58ef544a70091089236c9ab047b261e
Travoori <tharuniravoori1999@gmail.com>
2024-10-30 13:31:41 -0500

    Your message here

--- src/components/About.js
+++ src/components/About.js
@@ -6,11 +6,20 @@ function About() {
       <img src="/myphotoo.jpg" alt="Tharuni Ravoori" className="profile-img" />
       <h2>About Me</h2>
       <p>
-        Hi, I'm Tharuni Ravoori, a Computer Science graduate student at Saint
-        Louis University with experience in full-stack development. I’ve worked
-        as a Software Engineer at PwC, where I developed and maintained backend
-        and frontend components, and interned at DRDL, focusing on dynamic
-        interfaces and responsive web applications.
+        Hi! I'm Tharuni Ravoori, a dedicated and passionate Computer Science
+        graduate student at Saint Louis University with hands-on experience in
+        full-stack development and a strong foundation in both front-end and
+        back-end technologies. My journey in software engineering has led me to
+        work with diverse technologies, frameworks, and tools, and has allowed
+        me to contribute to impactful projects. Previously, I worked as a
+        Software Engineer at PwC, where I developed and maintained both backend
+        and frontend components, ensuring seamless user experiences and
+        efficient application performance.My technical expertise includes
+        working with languages such as Java, Python, and JavaScript, along with
+        frameworks like React. I am also well-versed in cloud platforms and
+        DevOps tools, which allow me to build, deploy, and manage scalable
+        applications. I am always eager to take on new challenges, learn
+        emerging technologies, and contribute to innovative projects.
       </p>
     </section>
   );
